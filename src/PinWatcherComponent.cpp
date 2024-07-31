78dc46378a0eae941be6d02b35c48480d752e792
Markus The <markus@mobzystems.nl>
2024-07-31 21:05:25 +0200

    Refactored pin watchers

--- src/PinWatcherComponent.cpp
+++ src/PinWatcherComponent.cpp
@@ -1,11 +1,9 @@
 #include "PinWatcherComponent.h"
 #include "logging.h"
 
-DigitalPinWatcherComponent::DigitalPinWatcherComponent(uint8_t pinNumber, uint8_t inputMode, void (*onValueChanged)(int, int)) :
-  Component("DigitalPin"),
-  _pinNumber(pinNumber),
-  _inputMode(inputMode),
-  _onValueChanged(onValueChanged)
+DigitalPinWatcherComponent::DigitalPinWatcherComponent(uint8_t pinNumber, uint8_t inputMode, std::function<void(int, int)> const onValueChanged, const char *name) :
+  PinWatcherComponent(name == NULL ? "DigitalPin" : name, pinNumber, onValueChanged),
+  _inputMode(inputMode)
 {}
 
 void DigitalPinWatcherComponent::setup() {
@@ -25,66 +23,10 @@ void DigitalPinWatcherComponent::loop() {
   }
 }
 
-DigitalClickWatcherComponent::DigitalClickWatcherComponent(uint8_t pinNumber, uint8_t inputMode, int detectionLevel, unsigned long maxInterval, void (*onClickDetected)(int numberOfclicks), int maxClicks) :
-  Component("ClickWatcher"), 
-  _pinNumber(pinNumber), 
-  _inputMode(inputMode),
-  _detectionLevel(detectionLevel),
-  _maxInterval(maxInterval),
-  _onClickDetected(onClickDetected),
-  _maxClicks(maxClicks),
-  _clickCount(0),
-  _lastTimestamp(0)
-{}
-
-void DigitalClickWatcherComponent::setup() {
-  // Configure the pin
-  pinMode(this->_pinNumber, this->_inputMode);
-  // Read its initial value
-  this->_lastValue = digitalRead(this->_pinNumber);
-  Log::logDebug("[%s] Pin %d is initially %d", this->name(), this->_pinNumber, this->_lastValue);
-}
-
-void DigitalClickWatcherComponent::loop() {
-  unsigned long timestamp = millis();
-
-  int currentValue = digitalRead(this->_pinNumber);
-  if (currentValue != this->_lastValue) {
-    // Do we now have the desired level (HIGH/LOW)?
-    if (currentValue == this->_detectionLevel) {
-      // Also: check that we're not within 100 ms of the last click
-      if (this->_clickCount != 0 && timestamp - this->_lastTimestamp < 100) {
-        Log::logTrace("[%s] Ignoring click on pin %d within %ld ms", this->name(), this->_pinNumber, timestamp - this->_lastTimestamp);
-
-      } else {
-        Log::logDebug("[%s] Click detected on pin %d (now %d)", this->name(), this->_pinNumber, currentValue);
-        this->_clickCount++;    
-        this->_lastTimestamp = timestamp;
-      }
-    } else {
-      Log::logTrace("[%s] Pin %d back to starting level", this->name(), this->_pinNumber);
-    }
-    // Always keep this value
-    this->_lastValue = currentValue;
-  }
-
-  // If we have detected one or more clicks, wait for the maximum interval to pass OR he maximum click count to be reached
-  // If this happens, register the clicks and reset the click count
-  if ((this->_maxClicks > 0 && this->_clickCount >= this->_maxClicks) || (this->_clickCount > 0 && timestamp > this->_lastTimestamp + this->_maxInterval)) {
-    Log::logDebug("[%s] Detected %d click", this->name(), this->_clickCount);
-    // We're done! Notify client
-    this->_onClickDetected(this->_clickCount);
-    // Reset the click count for the next series
-    this->_clickCount = 0;
-  }
-}
-
-AnalogPinWatcherComponent::AnalogPinWatcherComponent(uint8_t pinNumber, void (*onValueChanged)(uint16_t, uint16_t), uint16_t delta, unsigned long timeBetweenSamples) :
-  Component("AnalogPin"),
-  _pinNumber(pinNumber),
+AnalogPinWatcherComponent::AnalogPinWatcherComponent(uint8_t pinNumber, std::function<void(uint16_t, uint16_t)> const onValueChanged, uint16_t delta, unsigned long timeBetweenSamples) :
+  PinWatcherComponent("AnalogPin", pinNumber, onValueChanged),
   _delta(delta),
   _timeBetweenSamples(timeBetweenSamples),
-  _onValueChanged(onValueChanged),
   _lastReadTimestamp(millis())
 {}
 
@@ -109,4 +51,62 @@ void AnalogPinWatcherComponent::loop() {
   }
 }
 
+DigitalClickWatcherComponent::DigitalClickWatcherComponent(
+  uint8_t pinNumber, 
+  uint8_t inputMode, 
+  int detectionLevel, 
+  unsigned long maxInterval, 
+  std::function<void(int numberOfclicks)> const onClickDetected, 
+  int maxClicks
+) :
+  DigitalPinWatcherComponent(pinNumber, inputMode, [this](int previous, int currentValue) {
+    unsigned long timestamp = millis();
 
+    // Do we now have the desired level (HIGH/LOW)?
+    if (currentValue == this->_detectionLevel) {
+      // Also: check that we're not within 100 ms of the last click
+      if (this->_clickCount != 0 && timestamp - this->_lastTimestamp < 100) {
+        // Ignore this input change, it's not a click
+        Log::logTrace("[%s] Ignoring click on pin %d within %ld ms", this->name(), this->_pinNumber, timestamp - this->_lastTimestamp);
+      } else {
+        // We detected a click!
+        Log::logDebug("[%s] Click detected on pin %d (now %d)", this->name(), this->_pinNumber, currentValue);
+        this->_clickCount++; // Increment the number of clicks 
+        this->_lastTimestamp = timestamp; // Save this timestamp
+      }
+    } else {
+      Log::logTrace("[%s] Pin %d back to starting level", this->name(), this->_pinNumber);
+    }
+    // Always keep this value
+    this->_lastValue = currentValue;
+  }, "DigitalClick"),
+  _detectionLevel(detectionLevel),
+  _maxInterval(maxInterval),
+  _onClickDetected(onClickDetected),
+  _maxClicks(maxClicks),
+  _clickCount(0),
+  _lastTimestamp(0)
+{}
+
+void DigitalClickWatcherComponent::setup() {
+  DigitalPinWatcherComponent::setup();
+}
+
+void DigitalClickWatcherComponent::loop() {
+  DigitalPinWatcherComponent::loop();
+
+  unsigned long timestamp = millis();
+
+  // If we have detected one or more clicks, wait for the maximum interval to pass OR he maximum click count to be reached
+  // If this happens, register the clicks and reset the click count
+  if (
+    (this->_maxClicks > 0 && this->_clickCount >= this->_maxClicks) || // we reached the maximum number of clicks
+    (this->_clickCount > 0 && timestamp > this->_lastTimestamp + this->_maxInterval) // We have clicks and the maximum click time was reached
+  ) {
+    Log::logDebug("[%s] Detected %d click(s)", this->name(), this->_clickCount);
+    // We're done! Notify client
+    this->_onClickDetected(this->_clickCount);
+    // Reset the click count for the next series
+    this->_clickCount = 0;
+  }
+}
--- src/PinWatcherComponent.h
+++ src/PinWatcherComponent.h
@@ -3,54 +3,72 @@
 
 #include "Components.h"
 
-class DigitalPinWatcherComponent: public Component {
-  private:
+template<typename T> class PinWatcherComponent: public Component {
+  protected:
+    // The pin number to read
     uint8_t _pinNumber;
-    uint8_t _inputMode;
-    void (*_onValueChanged)(int, int);
+    // The last value read
+    T _lastValue;
 
-    int _lastValue;
+    std::function<void(T previous, T current)> const _onValueChanged;
+
+    PinWatcherComponent<T>(const char *name, uint8_t pinNumber, std::function<void(T previous, T current)> const onValueChanged):
+      Component(name),
+      _pinNumber(pinNumber),
+      _onValueChanged(onValueChanged)
+    {}
+};
+
+/*
+  Analog pin watcher. Calls the provided onValueChanged function when the analog input value changes
+*/
+class DigitalPinWatcherComponent: public PinWatcherComponent<int> {
+  protected:
+    // The input pin's input mode (e.g. INPUT_PULLUP)
+    uint8_t _inputMode;
 
   public:
-    DigitalPinWatcherComponent(uint8_t pinNumber, uint8_t inputMode, void (*onValueChanged)(int previous, int current));
+    DigitalPinWatcherComponent(uint8_t pinNumber, uint8_t inputMode, std::function<void(int previous, int current)> const onValueChanged, const char *name = NULL);
 
     void setup();
     void loop();
 };
 
-class DigitalClickWatcherComponent: public Component {
+/*
+  Analog pin watcher. Calls the provided onValueChanged function when the analog input value changes
+*/
+class AnalogPinWatcherComponent: public PinWatcherComponent<uint16_t> {
   private:
-    uint8_t _pinNumber;
-    uint8_t _inputMode;
-    int _detectionLevel; // HIGH or LOW
+    // The minimum value change that must occur before onValueChanged is called
+    uint16_t _delta;
+    // The minimum time between samples. When the analog pin is read too often, WiFi disconnects!
+    unsigned long _timeBetweenSamples;
+    // The time stamp of the last read
+    unsigned long _lastReadTimestamp;
+
+  public:
+    AnalogPinWatcherComponent(uint8_t pinNumber, std::function<void(uint16_t previous, uint16_t current)> const onValueChanged, uint16_t delta, unsigned long timeBetweenSamples = 20);
+
+    void setup();
+    void loop();
+};
+
+class DigitalClickWatcherComponent: public DigitalPinWatcherComponent {
+  private:
+    // register a click on HIGH or LOW
+    int _detectionLevel;
+    // After this number of ms after the first click we always detect one
     unsigned long _maxInterval;
-    void (*_onClickDetected)(int numberOfclicks);
+    std::function<void(int numberOfclicks)> const  _onClickDetected;
     // Always register immediately when this number of clicks detected. 0: no maximum
     int _maxClicks;
-
-    int _lastValue;
+    // The number of clicks detected so far
     int _clickCount;
+    // The time stamp of the last detected click
     unsigned long _lastTimestamp;
 
   public:
-    DigitalClickWatcherComponent(uint8_t pinNumber, uint8_t inputMode, int detectionLevel, unsigned long maxInterval, void (*onClickDetected)(int numberOfclicks), int maxClicks = 0);
-
-    void setup();
-    void loop();
-};
-
-class AnalogPinWatcherComponent: public Component {
-  private:
-    uint8_t _pinNumber;
-    uint16_t _delta;
-    unsigned long _timeBetweenSamples;
-    void (*_onValueChanged)(uint16_t, uint16_t);
-    unsigned long _lastReadTimestamp;
-
-    uint16_t _lastValue;
-
-  public:
-    AnalogPinWatcherComponent(uint8_t pinNumber, void (*onValueChanged)(uint16_t previous, uint16_t current), uint16_t delta, unsigned long timeBetweenSamples = 20);
+    DigitalClickWatcherComponent(uint8_t pinNumber, uint8_t inputMode, int detectionLevel, unsigned long maxInterval, std::function<void(int numberOfclicks)> const onClickDetected, int maxClicks = 0);
 
     void setup();
     void loop();
