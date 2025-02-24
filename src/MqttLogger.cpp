b6a57e4f949835a86252d67ebdce8c8fa9400f38
Markus The <markus@mobzystems.nl>
2025-02-24 16:12:40 +0100

    Attempt to do backlog messaging better

--- src/MqttLogger.cpp
+++ src/MqttLogger.cpp
@@ -15,7 +15,7 @@ MqttLogger::MqttLogger(MqttComponent *mqtt, const char *topic, int max_size, Log
       Serial.println(String(">>> MQTT: ") + message);
 #endif
       // Push message onto backlog, including timestamp
-      _backlog.push(Log::getTimeStamp() + message);
+      _backlog.push(message);
 
       // Remove excess entries from backlog
       while ((int)_backlog.size() > _max_size)
@@ -27,10 +27,16 @@ MqttLogger::MqttLogger(MqttComponent *mqtt, const char *topic, int max_size, Log
   _max_size(max_size)
 {}
 
-/*
-* Send the back-log to the Mqtt broker
-*/
+/**
+ * Send the back-log to the Mqtt broker
+ * NOT ANYMORE: If we have a continuous connection, send the message without a timestamp. The timestamp will be added by the logger.
+ * If we had an MQTT connection interruption, DO send thecurrent timestamp before the message. The log message
+ * already contains its own time stamp, so it will eventually look like "<publish-TS> [<Log-TS>] Message", i.e. the time stamp
+ * of the PUBLISHING of the message is included between square brackets
+ */
 void MqttLogger::loop() {
+  // static bool missedBacklog = false;
+
   // If there is a backlog, send it here:
   if (!_backlog.empty()) {
     // Are we connected?
@@ -40,12 +46,21 @@ void MqttLogger::loop() {
 #ifdef LOG
       Serial.println("--- Start of backlog");
 #endif
+      // String timeStamp = Log::getTimeStamp();
+      // timeStamp.trim();
+      // timeStamp = String("[") + timeStamp + String("] ");
+
       while (!_backlog.empty()) {
-        auto msg = _backlog.front();
+        auto message = _backlog.front();
 #ifdef LOG
         Serial.println(msg);
 #endif
-        _mqtt->mqttClient()->publish(_topic.c_str(), msg.c_str());
+        // // Log with extra time stamp if we have missed backlog entries
+        // if (missedBacklog) {
+        //   _mqtt->mqttClient()->publish(_topic.c_str(), (timeStamp + message).c_str());
+        // } else {
+          _mqtt->mqttClient()->publish(_topic.c_str(), message.c_str());
+        // }
         _backlog.pop();
       }
 #ifdef LOG
@@ -53,6 +68,13 @@ void MqttLogger::loop() {
 #endif
       // Allow re-entry
       _blocked = false;
+      // // Solved missing backlog
+      // missedBacklog = false;
     }
+    // else {
+    //   // If not connected, signal we have a missed backlog. This will cause timestamps to be added the next time we
+    //   // have an MQTT connection
+    //   missedBacklog = true;
+    // }
   }
 }
